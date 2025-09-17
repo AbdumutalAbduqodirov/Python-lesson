ad0cfea33ae7e70698dcbeeacb283d44c196bb9d
AbdumutalAbduqodirov <kaspeniski97@gmail.com>
2025-09-17 18:28:20 +0500

    6_dars <Lug'at>

---
+++ 6_dars/main.py
@@ -0,0 +1,22 @@
+
+# son = [1,2,3,4,5,6]
+# print(son[0])
+# ********* Lug'at ***********
+# car = {"model":"bmw",'color':'black', 'year':'1996'}
+# print(car['model'])
+# print(f"Mening mashinam {car['model']} uning rangi {car['color']}, u {car['year']} - yilda ishlab chiqarilgan eng kuchli avtomabil.")
+# res = input("Ingilizcha so'z kriting: ")
+# ing_uzb = {'book': 'Kitob', 'hello': 'Salom', 'world': 'dunyo'}
+# if res.lower() == 'book':
+#     print(ing_uzb['book'])
+# elif res.lower() == 'hello':
+#     print(ing_uzb['hello'])
+# elif res.lower() == 'world':
+#     print(ing_uzb['world'])
+# else:
+#     print("Bizning lug'atda xozircha bunday so'z mavjud emas.")
+
+# res = input("Ingilizcha so'z kriting: ")
+# ing_uzb = {'book': 'Kitob', 'hello': 'Salom', 'world': 'dunyo'}
+# soz = ing_uzb.get(res.lower() ,"Bizning lug'atda xozircha bunday so'z mavjud emas.")
+# print(soz)
